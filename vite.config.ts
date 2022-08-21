7e224068505d875e3ee18f4e441b9cf5f9e6b3fe
AckermanNan <1178809645@qq.com>
2022-08-21 16:50:58 +0800

    初始化

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import Components from "unplugin-vue-components/vite";
+import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
@@ -7,7 +9,12 @@ export default defineConfig(({ command, mode }) => {
   return {
     // root: "",
     base: "/",
-    plugins: [vue()],
+    plugins: [
+      vue(),
+      Components({
+        resolvers: [AntDesignVueResolver()],
+      }),
+    ],
     resolve: {
       alias: {
         "@config": "/src/config",
