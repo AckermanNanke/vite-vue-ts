e7c4926422f8f6df547a5f66476439feb1b44906
AckermanNan <1178809645@qq.com>
2022-08-27 15:56:53 +0800

    github-pages创建

---
+++ assets/register.4eb7cad9.js
@@ -0,0 +1,1 @@
+import{d as e,r,c as n,o}from"./index.229c3f41.js";const p=e({__name:"register",props:{msg:null},setup(s){return r(0),(t,a)=>(o(),n("h1",null,"\u6CE8\u518C"))}});export{p as default};
