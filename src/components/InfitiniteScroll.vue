7ad13a1ce114c618af65ac94123a3f474eb227eb
AckermanNan <1178809645@qq.com>
2023-02-06 08:43:58 +0800

    下拉加载组件组件

--- src/components/InfitiniteScroll.vue
+++ src/components/InfitiniteScroll.vue
@@ -12,24 +12,22 @@ const props = defineProps<{
  * 定义抛出事件类型
  */
 const emit = defineEmits<{
-  (e: "pullDown"): void;
-  (e: "pullUp"): void;
+  (e: "pullUp", func: () => void): void;
+  (e: "pullDown", func: () => void): void;
 }>();
 
-const loading = ref(false); //是否加载
+const upLoading = ref(false); //是否加载中
+const downLoading = ref(false); //是否刷新中
 const scrollEl = ref(); //组件实例
 const TSParams = ref<TouchEvent | undefined>();
 const TEParams = ref<TouchEvent | undefined>();
 
 function touchstart(e: TouchEvent) {
-  if (loading.value) return false;
+  if (upLoading.value || downLoading.value) return false;
   TSParams.value = e;
-  console.log("touchstart====================");
-  console.log(e);
-  console.log("touchstart====================");
 }
-function touchend(e: TouchEvent) {
-  if (loading.value) return false;
+async function touchend(e: TouchEvent) {
+  if (upLoading.value || downLoading.value) return false;
   TEParams.value = e;
   const Y =
     TEParams.value!.changedTouches[0]?.pageY - TSParams.value!.touches[0].pageY; // 垂直移动距离
@@ -37,35 +35,38 @@ function touchend(e: TouchEvent) {
 
   // 下拉刷新
   if (Y - props.distance >= 0) {
-    loading.value = true;
-    emit("pullUp");
-    setTimeout(() => {
-      loading.value = false;
-    }, 2000);
+    downLoading.value = true;
+    emit("pullDown", () => {
+      loadEnd();
+    });
   }
   /**
-   * 下拉加载
+   * 上拉加载
    * 整个元素高度-可视区域高度-滑动高度
    * 滑动距离超过指定值进行上拉加载
    */
   if (
-    scrollEl.value.scrollHeight -
-      scrollEl.value.clientHeight -
-      scrollEl.value.scrollTop >=
-    props.distance
+    Math.abs(
+      scrollEl.value.scrollHeight -
+        scrollEl.value.clientHeight -
+        scrollEl.value.scrollTop
+    ) < props.distance
   ) {
     if (-Y >= props.distance) {
-      emit("pullDown");
+      upLoading.value = true;
+      emit("pullUp", () => {
+        loadEnd();
+      });
     }
   }
-  console.log("touchend-----------------");
-  console.log(Y);
-  console.log(scrollEl);
-  console.log("touchend-----------------");
 }
 // 触摸移动事件
 function touchmove(e: TouchEvent) {
-  if (loading.value) return false;
+  if (upLoading.value || downLoading.value) return false;
+}
+function loadEnd() {
+  upLoading.value = false;
+  downLoading.value = false;
 }
 onMounted(() => {});
 </script>
@@ -79,12 +80,17 @@ onMounted(() => {});
     @touchmove="touchmove"
   >
     <slot name="no-data"> 没有数据 </slot>
-    <slot name="loading">
-      <div class="f-infitinite-scroll-loading" :class="{ active: loading }">
+    <slot name="downLoading">
+      <div class="f-infitinite-scroll-loading" :class="{ active: downLoading }">
         加载中...
       </div>
     </slot>
     <slot></slot>
+    <slot name="upLoading">
+      <div class="f-infitinite-scroll-loading" :class="{ active: upLoading }">
+        上拉加载更多数据
+      </div>
+    </slot>
     <slot name="no-more"> 这已经是我的底线了 </slot>
   </section>
 </template>
@@ -97,10 +103,10 @@ onMounted(() => {});
   &-loading {
     &.active {
       height: 60px;
-      line-height: 60px;
-      color: @text-tip;
-      text-align: center;
     }
+    line-height: 60px;
+    color: @text-tip;
+    text-align: center;
     overflow: hidden;
     height: 0;
     transition: height cubic-bezier(0.18, 0.89, 0.32, 1.28) 0.8s;
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,17 @@
 <script setup lang="ts">
 import { ref } from "@vue/runtime-core";
 const arr = ref<number[]>([]);
-function scroll() {
-  arr.value!.push(1);
+function pullUp(callback: () => void) {
+  setTimeout(() => {
+    arr.value!.push(1);
+    // callback();
+  }, 2000);
+}
+function pullDown(callback: () => void) {
+  console.log(callback);
+  setTimeout(() => {
+    callback();
+  }, 2000);
 }
 </script>
 
@@ -10,7 +19,7 @@ function scroll() {
   <div style="height: 100vh; display: flex; flex-direction: column">
     <section style="height: 20vh; width: 100%; background: #000000"></section>
     <section style="width: 100%; flex: 1; max-height: 60vh">
-      <InfitiniteScroll :distance="0" @pullDown="scroll">
+      <InfitiniteScroll :distance="20" @pullUp="pullUp" @pullDown="pullDown">
         <div class="scroll" v-for="item in arr" :key="item"></div>
       </InfitiniteScroll>
     </section>
