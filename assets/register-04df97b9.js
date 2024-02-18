21e4947cd4fd094ca6bc0ca3a060461d484fbf77
AckermanNan <1178809645@qq.com>
2024-02-18 21:12:43 +0800

    github-pages创建

---
+++ assets/register-04df97b9.js
@@ -0,0 +1,1 @@
+import{e}from"./index-23ba9e27.js";function s(r){return e.instance.post("register",r)}export{s as r};
--- assets/register-ffaff0a9.js
+++
@@ -1,1 +0,0 @@
-import{e}from"./index-d6c8d9c5.js";function s(r){return e.instance.post("register",r)}export{s as r};
